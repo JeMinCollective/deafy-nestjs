39e63394e269fb43d285c8a3fbe7b63436dcbc8a
Lechelyn <lechelynloyao@gmail.com>
2026-04-10 23:07:13 +0800

    Allowed the cloudinary site for images.

--- components/sections/team-section.tsx
+++ components/sections/team-section.tsx
@@ -71,7 +71,7 @@ export function TeamSection() {
           </p>
         </div>
 
-        <div className="mx-auto grid max-w-5xl gap-8 sm:grid-cols-2 lg:grid-cols-4">
+        <div className="mx-auto grid max-w-7xl gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5">
           {teamMembers?.map((member) => (
             <article
               key={member.name}
